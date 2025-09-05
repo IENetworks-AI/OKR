d9823494db2eed99a46107a95fc5310292ee7ce4
mikias1219 <mikiyasabate003@gmail.com>
2025-09-05 11:34:18 +0300

    fixed

--- scripts/airflow_bootstrap.sh
+++ scripts/airflow_bootstrap.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
+AIRFLOW_BIN="${AIRFLOW_BIN:-/usr/local/bin/airflow}"
+
 # Run DB migrations if requested
 if [[ "${_AIRFLOW_DB_MIGRATE}" == "true" ]]; then
   echo "Running airflow db upgrade..."
-  airflow db upgrade
+  "${AIRFLOW_BIN}" db upgrade
 fi
 
 # Create admin user if requested
 if [[ "${_AIRFLOW_WWW_USER_CREATE}" == "true" ]]; then
   echo "Creating Airflow admin user..."
-  airflow users create \
+  "${AIRFLOW_BIN}" users create \
     --username "${_AIRFLOW_WWW_USER_USERNAME:-admin}" \
     --password "${_AIRFLOW_WWW_USER_PASSWORD:-admin}" \
     --firstname Admin \
